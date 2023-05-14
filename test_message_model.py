22e8583974131b0f066dd4dc34e072198a108013
Seth Vargas <shvargas01@gmail.com>
2023-05-14 16:10:16 -0500

    Basic model is working as expected

--- test_message_model.py
+++ test_message_model.py
@@ -1,4 +1,4 @@
-"""User model tests."""
+"""Message model tests."""
 
 # run these tests like:
 #
@@ -8,7 +8,7 @@
 import os
 from unittest import TestCase
 
-from models import db, User, Message, Follows
+from models import db, User, Message, Follows, Likes
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -29,8 +29,8 @@ db.drop_all()
 db.create_all()
 
 
-class UserModelTestCase(TestCase):
-    """Tests user models."""
+class MessageModelTestCase(TestCase):
+    """Tests message models."""
 
     def setUp(self):
         """Create test client, add sample data."""
@@ -42,3 +42,29 @@ class UserModelTestCase(TestCase):
         Follows.query.delete()
 
         self.client = app.test_client()
+        
+        
+    def test_messsage_model(self):
+        """ Does basic model work? """
+        
+        u = User(
+            email="test@test.com",
+            username="testuser",
+            password="HASHED_PASSWORD"
+        )
+        
+        db.session.add(u)
+        db.session.commit()
+
+        m = Message(
+            text = "This is a test",
+            user_id = u.id
+        )
+        
+        db.session.add(m)
+        db.session.commit()
+        
+        self.assertEqual(len(u.messages), 1)
+        
+        
+    

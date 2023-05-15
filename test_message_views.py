76ac471af08d704ecc8de9171e9357d4887806ca
Seth Vargas <shvargas01@gmail.com>
2023-05-15 15:28:34 -0500

    Finished testing message views

--- test_message_views.py
+++ test_message_views.py
@@ -25,7 +25,7 @@ from app import app, CURR_USER_KEY
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
 # and create fresh new clean test data
-
+db.drop_all()
 db.create_all()
 
 # Don't have WTForms use CSRF at all, since it's a pain to test
@@ -39,8 +39,12 @@ class MessageViewTestCase(TestCase):
     def setUp(self):
         """Create test client, add sample data."""
 
-        User.query.delete()
-        Message.query.delete()
+        db.session.rollback()
+
+        db.drop_all()
+        db.create_all()
+        # User.query.delete()
+        # Message.query.delete()
 
         self.client = app.test_client()
 
@@ -71,3 +75,42 @@ class MessageViewTestCase(TestCase):
 
             msg = Message.query.one()
             self.assertEqual(msg.text, "Hello")
+
+    def test_show_message(self):
+        """ Test that user can see specific message """
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.testuser.id
+
+            msg = Message(text="TEST", user_id=self.testuser.id)
+
+            db.session.add(msg)
+            db.session.commit()
+
+            resp = c.get(f"/messages/{msg.id}")
+
+            # as_test parameter is important!
+            html = resp.get_data(as_text=True)
+
+            text = '<p class="single-message">TEST</p>'
+
+            self.assertIn(text, html)
+            self.assertEqual(resp.status_code, 200)
+
+    def test_messages_destroy(self):
+        """ Tests that deleting a message behaves as expected """
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.testuser.id
+
+        msg = Message(text="TEST", user_id=self.testuser.id)
+
+        db.session.add(msg)
+        db.session.commit()
+
+        resp = c.post(f"/messages/{msg.id}/delete")
+
+        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(resp.location, 'http://localhost/users/1')
